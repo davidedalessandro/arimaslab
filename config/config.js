d652c4b4eaeaad25dcc980b9fa711726a4eb4000
Davide D'Alessandro <davidedale4@gmail.com>
2022-10-13 16:33:04 +0200

    first commit

---
+++ config/config.js
@@ -0,0 +1,32 @@
+/* eslint-disable prettier/prettier */
+module.exports = {
+    title: "Arimasab",
+    titleTemplate: "Arimaslab - Your digital Agency",
+    description: "arimaslab is your next digital agency",
+    author: "Arimaslab",
+    twitterUsername: "@arimaslab",
+    keywords: [
+        "bootstrap",
+        "html",
+        "react",
+        "gatsby",
+        "template",
+        "blog",
+        "creative",
+    ],
+    image: "banner_image.png",
+    siteUrl: "https://arimaslab.com",
+    pathPrefix: "/arimaslab",
+    localUrl: "http://localhost:8000",
+    canonical: "https://arimaslab.com",
+    social: {
+        facebook: "#",
+        twitter: "#",
+        linkedin: "#",
+    },
+    siteLanguage: "en",
+    favicon: "src/assets/images/favicon.png", // Used for manifest favicon generation
+    shortName: "bunzo", // shortname for manifest. *Must* be shorter than 12 characters
+    themeColor: "#5E60E7",
+    backgroundColor: "#fff",
+};
