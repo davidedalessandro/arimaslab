cdbb3b0bdeb0eb93795146ae36f7ad91dffafcad
Davide D'Alessandro <davidedale4@gmail.com>
2022-10-14 09:38:37 +0200

    pagina blog dinamica

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,40 +1,42 @@
-import React from 'react'
-import { graphq, link } from 'gatsby'
-import Layout from '../components/layout'
-import { title } from '../../config/config'
+import * as React from 'react'
+import { Link, graphql } from 'gatsby'
+import Layout from '../../components/layout'
+import Seo from '../../components/seo'
 
-export const query = graphql
-    query {
-        wpgraphql {
-            posts {
-                nodes {
-                    Id
-                    title
-                    uri
-                    exerpt
-                }
-            }
-        }
-    }
-
-
-const Blog = ({ data }) => {
-
-    const post = data.wpgraphql.posts.nodes
-
-    return (
-        <layout>
-            {posts.map(post =>(
-                <article key={post.id}>
-                    <h2>
-                        <link to={/blog/${post.uri}} dangerouslysetinnerHTML={{ __html: post.title}} />
-                        <div dangerouslySetInnerHTML={{ __html: post.exerpt}} />
-
-                    </h2>
-                </article>
-            ))}
-        </layout>
-    )
+const BlogPage = ({ data }) => {
+  return (
+    <Layout pageTitle="My Blog Posts">
+      {
+        data.allMdx.nodes.map(node => (
+          <article key={node.id}>
+            <h2>
+              <Link to={`/blog/${node.frontmatter.slug}`}>
+                {node.frontmatter.title}
+              </Link>
+            </h2>
+            <p>Posted: {node.frontmatter.date}</p>
+          </article>
+        ))
+      }
+    </Layout>
+  )
 }
 
-export default blog
+export const query = graphql`
+  query {
+    allMdx(sort: {fields: frontmatter___date, order: DESC}) {
+      nodes {
+        frontmatter {
+          date(formatString: "MMMM D, YYYY")
+          title
+          slug
+        }
+        id
+      }
+    }
+  }
+`
+
+export const Head = () => <Seo title="My Blog Posts" />
+
+export default BlogPage
